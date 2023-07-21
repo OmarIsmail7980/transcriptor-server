45a2c9fc0e4ba26ad5788a2fd7c088f6961f85d5
Omar Ismail <omar.business7980@gmail.com>
2023-07-21 12:37:07 -0500

    code update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const transcribe = require("./routes/transcribe");
-const test2 = require("./routes/test2");
 const PORT = 8091;
 
 const app = express();
@@ -10,8 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/transcribe", transcribe);
-app.use("/test", test2);
-
 
 app.get("/", (req, res) => {
   res.send("App is up and running");
